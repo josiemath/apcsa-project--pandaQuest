551f3864ce960d7d2650ebf338df7327aebe2687
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-06 21:53:33 -0800

    Implement click sound for Purple Panda
    Added click sound effect when the Purple Panda is clicked.

--- PurplePanda.java
+++ PurplePanda.java
@@ -6,6 +6,7 @@ import greenfoot.*;
 public class PurplePanda extends Actor
 { 
     private static final GreenfootImage NORMAL_IMAGE = new GreenfootImage("purplePanda.PNG");
+    private GreenfootSound clickSound = new GreenfootSound("SelectSound.mp3");
     
     /**
      * Constructor - Sets purple panda image.
@@ -23,6 +24,7 @@ public class PurplePanda extends Actor
         if (Greenfoot.mouseClicked(this))
         {
             Levels.color = 5;
+            clickSound.play();
             return; 
         }
     }    
